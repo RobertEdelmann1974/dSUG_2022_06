9c62f1517859358a8e24c22930d54d47801e397f
Robert Edelmann <Mail@Robert-Edelmann.de>
2022-06-30 03:39:51 +0200

    Scope für Einstellungen Filterung

---
+++ dSUGPopupFilter/popupfilter.js
@@ -0,0 +1,78 @@
+/**
+ * @private 
+ * @properties={typeid:24,uuid:"26C0E710-85EA-4236-8D91-ED5017D48D4F"}
+ */
+function setupPopupFilter() {
+	var ngFilterKey = scopes.svySecurityManager.getKey('NG_Filter')
+	if (ngFilterKey) {
+		scopes.svySecurityManager.addRuntimeKey(ngFilterKey.keyId, ngFilterKey.name);
+	}
+	if (application.isInDeveloper() || scopes.BauProCheck.isUserTesting(scopes.BauProCheckDefaults.userId, scopes.BauProCheckDefaults.organisationId)) {
+		var ngFilterTestKey = scopes.svySecurityManager.getKey('NG_Filter_Test')
+		scopes.svySecurityManager.addRuntimeKey(ngFilterTestKey.keyId, ngFilterTestKey.name);
+	} else {
+		var noNGFilterKey = scopes.svySecurityManager.getKey('No_NG_Filter')
+		if (noNGFilterKey) {
+			scopes.svySecurityManager.addRuntimeKey(noNGFilterKey.keyId, noNGFilterKey.name);
+		}
+	}
+	scopes.svyPopupFilter.LOCALE = {
+		filterPopupMenu: {
+			addFilter: 'Filter hinzufügen'
+		},
+		svyCheckPopupFilter : {
+			labelChecked: 'Ja',
+			labelUnchecked: 'Nein'
+		},
+		svyDatePopupFilter : {
+			labelYesterday: 'Gestern',
+			labelLastWeek: 'Letzte Woche',
+			labelLastMonth: 'Letzter Monat',
+			labelNextYear: 'Nächstes Jahr',
+			labelTitle: 'Datum',
+			labelToday: 'Heute',
+			labelTomorrow: 'Morgen',
+			labelThisWeek: 'Diese Woche',
+			labelNextWeek: 'Nächste Woche',
+			labelThisMonth: 'Dieser Monat',
+			labelNextMonth: 'Nächster Monat',
+			labelThisYear: 'Dieses Jahr',
+			labelLastYear: 'Letztes Jahr',
+			operator: {
+				EQUALS: 'Genaues Datum',
+				GREATER_EQUAL: 'Nach',
+				SMALLER_EQUAL: 'Vor',
+				BETWEEN: 'Zwischen'
+			}
+		},
+		svyTokenPopupFilter : {
+			searchbox: {placeholderText: 'Filter-Wert eingeben...'},
+			labelRemoveAll: 'Alle entfernen'
+		},
+		svySelectPopupFilter : {
+			searchText: { placeholderText: 'Suchen...' },
+			labelSelectAll: 'Alle auswählen',
+			labelDeselectAll: 'Alle entfernen',
+			titleText: 'Wert'
+		},
+		svyIntegerPopupFilter : {
+			labelTitle: 'Filter-Wert eingeben...',
+			labelEqualTo: 'Genau',
+			labelGreater: 'Größer als',
+			labelSmaller: 'Kleiner als',
+			labelBetween: 'Zwischen'
+		},
+		svyNumberPopupFilter : {
+			labelTitle: 'Filter-Wert eingeben...',
+			labelEqualTo: 'Genau',
+			labelGreater: 'Größer als',
+			labelSmaller: 'Kleiner als',
+			labelBetween: 'Zwischen'
+		}
+	};
+	scopes.svyToolbarFilter.setPopupRendererForm(scopes.svyToolbarFilter.FILTER_TYPES.SELECT, forms.dsugPopupFilterSelect);
+	scopes.svyToolbarFilter.setPopupRendererForm(scopes.svyToolbarFilter.FILTER_TYPES.DATE, forms.dsugPopupFilterDate);
+	scopes.svyToolbarFilter.setPopupRendererForm(scopes.svyToolbarFilter.FILTER_TYPES.NUMBER, forms.dsugPopupFilterNumber);
+	scopes.svyToolbarFilter.setPopupRendererForm(scopes.svyToolbarFilter.FILTER_TYPES.TOKEN, forms.dsugPopupFilterToken);
+	scopes.svyToolbarFilter.setPopupDefaultOperator(scopes.svyToolbarFilter.FILTER_TYPES.TOKEN, scopes.svyPopupFilter.OPERATOR.LIKE_CONTAINS);
+}
